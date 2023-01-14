2418a3052f03f7a34a828434348caefe2188bc7f
ttraintimes <ttraintimes@gmail.com>
2023-01-14 10:31:16 +0000

    1

---
+++ My project/Assets/scripts/GameManamge.cs
@@ -0,0 +1,37 @@
+﻿using RootMotion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManamge : MonoBehaviour
+{
+    public AudioSource TipAudio;
+    public Text text;
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
+    public GameObject[] ddf;
+    void Start()
+    {
+    }
+
+    public Text ttd;
+    [HideInInspector]
+    public  int a=0;
+    [HideInInspector]
+    public int a2 = 0;
+    public void DF()
+    {
+        a++;
+        ttd.text = a+"/3";
+        TipAudio.Play();
+        anis.Play("1");
+    }
+    public Animator anis;
+    void Update()
+    {
+    }
+   
+}
